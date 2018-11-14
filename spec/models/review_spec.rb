a4e1e0d785e6dbefd04d0de8f93bb77ff41a41ed
Jacqueline Chenault <adigitalnative@gmail.com>
2018-11-14 12:36:03 -0500

    Numericality validations in place for Ratings

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,13 +12,32 @@ RSpec.describe Review, type: :model do
     expect(review).to_not be_valid
   end
 
-  it "is invalid without a rating" do
-    review.rating = nil
-    expect(review).to_not be_valid
-  end
+  
 
   it "is invalid without a reviewer" do
     review.reviewer = nil
     expect(review).to_not be_valid
+  end
+
+  context ".rating" do
+    it "is invalid without a rating" do
+      review.rating = nil
+      expect(review).to_not be_valid
+    end
+
+    it "is invalid with a string for rating" do
+      review.rating = "foo"
+      expect(review).to_not be_valid
+    end
+
+    it "is invalid with a rating over 5" do
+      review.rating = 6
+      expect(review).to_not be_valid
+    end
+
+    it "is invalid with a rating under 1" do
+      review.rating = 0
+      expect(review).to_not be_valid
+    end
   end
 end
